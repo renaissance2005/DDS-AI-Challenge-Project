0df229b89dbb7979069e74d9f5b9c5e9b9c183bf
David Lau <wah_lau2005@yahoo.com.sg>
2024-10-06 12:11:45 +0800

    These file are overtaken by new ones
    Not needed

--- embeddings.py
+++
@@ -1,7 +0,0 @@
-
-from langchain_community.embeddings import OllamaEmbeddings
-
-def generate_embeddings():
-    """Generate embeddings using the OllamaEmbeddings model"""
-    embeddings = OllamaEmbeddings(model="nomic-embed-text")
-    return embeddings
